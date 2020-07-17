676ef75eee60e2a495b8d369b3fedb4dc2fe6e58
davidedsams <davidedsams@gmail.com>
2020-07-16 23:59:03 -0400

    Updated README to be more than just a proposal. Added a seperate insctructions page for players not looking at the code. Got rid of all the nonfunctioning and commented out code.

--- README.md
+++ README.md
@@ -1,35 +1,52 @@
-# Project Proposal Example
+# Simon Slams
 
-## Project Choice
-
-- Simon
-
-## Project Description
-
-My app is called Simon. It's a game app for players who want to play Simon online. It's exactly like the popular game of the same name. Simon (the computer) starts the game by pressing a button on the board and the player has to match the button. The game quickly turns from easy to challenging as the pattern of buttons pressed increases by one each time a turn is completed and the player has to remember an increasingly complex pattern. The game ends whenever the player fails to match the pattern of the computer or the player matches 5 buttons in a row.
-
-## Wire Frames
-
-**Initial Landing View**
+A game app for players who want to play Simon online. It's exactly like the popular game in real life except that players also get to enjoy commentary by Simon Cowell as they play. The computer starts the game by pressing a square on the board and the player has to match the square in turn. The game quickly turns from easy to challenging as the pattern of squares pressed increases by one each time a turn is completed and the player has to remember an increasingly complex pattern. The game ends whenever the player fails to match the pattern of the computer or the player matches 8 squares in a row.
 
 ![Screen Shot 2020-07-10 at 11 52 49 AM](https://user-images.githubusercontent.com/33344527/87173520-e83d2300-c2a3-11ea-9b03-e7818da4d624.png)
 
-<!-- **Results View** -->
-
 ## User Stories
 
-#### MVP Goals
+#### Bronze Level Goals (These goals are all working on the current version.)
 
 - As a player, I would like the rules and instructions to be easy to find on the page so that I know how to play the game and therefore enjoy it more.
 - As a player, I would like to be able to easily see which buttons on the board are being pressed by either the computer or myself so that I can remember the pattern better and get to higher levels.
 - As a player, I would like all the buttons to be big enough and far enough apart so that I don't accidentally press the wrong one during game play.
 - As a player who requires assistive technologies, I would like accessibility features so that I'm not left out of enjoying the game.
 
-#### Stretch Goals
+#### Silver Level Goals (These goals and the rest of the levels will be coming soon.)
 
+- As a player, I would like to hear an audible sound just like in the real game when either the computer or myself presses a button so that I can remember the pattern more easily and also enter it in more accurately. And it's just so much more fun with sound too!
 - As a player, I would like a button on the board to replay the last pattern in case I want to hear it again before I attempt to match it.
 - As a player, I would like the option to change the speed at which the computer plays the pattern so that I can learn to play the game better and have a fun time.
-- As a player, I would like to hear an audible sound just like in the real game when either the computer or myself presses a button so that I can remember the pattern more easily and also enter it in more accurately. And it's just so much more fun with sound too!
 - As a player, I would like the option to turn the sound off so that I can play the game silently on my device and not have to worry about muting my phone or computer completely.
-- As a player, I would like the appearance of the game to switch from the pocket version on small and medium size screens to the classic bigger version for desktop screens.
-- As a player, I would like a video of Simon from American Idol to pop up in the corner at intervals throughout the game saying different things depending on my progress or lack thereof.
+- As a player, I would like the appearance of the game to switch from the pocket version on small and medium size screens to the classic bigger version for desktop screens. This will make my experience on any screen size more enjoyable.
+
+#### Gold Level Goals
+
+- As a player, I would like a video of Simon Cowell to pop up in the corner at intervals throughout the game saying different things depending on my progress or lack thereof. If I win three times in a row I would like a video of Mr. Cowell being brought to tears at how well I just played the game. This will make me want to play the game more often.
+
+#### Platinum Level Goals
+
+- As a player, if I win the game, I would like there to be some sort of dramatic lighting and music effects in the style of a game show to make the game much more fun.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Web Audio API
+
+## Installation Instructions
+
+For now just fork, clone or just copy and paste the code into the editor of your choice to play the game in any modern web browser.
+
+If you'd like to play the game right away though, simply follow this link .
+
+## Contribute
+
+
+
+
+## Acknowledgements
+
+- Awesome oscillator stuff learned in part through https://middleearmedia.com/web-audio-api-oscillators/.
--- script.js
+++ script.js
@@ -1,41 +1,5 @@
 // -----------------------------------------------------------------
-// How To Play Simon Slams
-// -----------------------------------------------------------------
-//
-// Object of Game: Correctly repeat longer and longer sequences
-//				   of signals.
-//
-// 1-Player Game
-//
-// 1. Press the start button.
-// 2. Simon will give the first signal. Repeat the signal by
-//    pressing the same color square.
-// 3. Simon will duplicate the first signal and add one. Repeat
-//    these two signals by pressing the same color squares,
-//    in order.
-// 4. Simon will duplicate these first two signals and add one.
-// 5. Continue playing as long as you can repeat each sequence
-//    of signals correctly.
-// 6. If you fail to repeat a sequence exactly, or if you take
-//    more than 5 seconds to repeat a signal, Simon responds with
-//    an insult and the game ends.
-//
-// How To Win
-//
-// Repeat 10 sequences and Simon will actually compliment and
-// congratulate you. I know, right?! (Sorta-Secret Bonus: If you
-// win 3 games in a row you'll make Simon cry!)
-//
-// 2 or More Player Game
-//
-// Players may team up against Simon or compete with each other.
-//
-// 1. Decide who goes first.
-// 2. Play proceeds as described above except players take turns
-//    repeating Simon's signals.
-
-// -----------------------------------------------------------------
-// How To Write The Dang Game
+// How To Write Simon Slams
 // -----------------------------------------------------------------
 //
 // This game needs a random number to be chosen and then pushed to
@@ -62,36 +26,33 @@
 // goes back to how it was on loading.
 
 // -----------------------------------------------------------------
-// THE CODE
-// -----------------------------------------------------------------
-// -----------------------------------------------------------------
 // Variables
 // -----------------------------------------------------------------
-
-// let start; = true or false
 
 let number;
 let computerPattern = [];
 let playerPattern = [];
 let userClick = 0;
 let turnCounter = 0;
-// let turnCounter = [];
-// let win; = true or false
-// let match; = true or false
+
+let context = new AudioContext();
+let oscillator;
+
+let start = document.querySelector('.start');
+let gameBoard = document.querySelector('.gameBoard');
 
 // -----------------------------------------------------------------
 // Functions
 // -----------------------------------------------------------------
 
-let start = document.querySelector('.start');
 start.addEventListener('click', handleStartButton);
 function handleStartButton() {
 	computerPattern = [];
 	addOne();
 }
 
-// This game needs a random number to be chosen and then pushed to
-// an array.
+// This game needs a random number to be chosen and then pushed to the computerPattern array.
+
 function randomNumber() {
 	number = Math.floor(Math.random() * 4 + 1);
 	return number;
@@ -100,38 +61,20 @@ function randomNumber() {
 function addOne() {
 	computerPattern.push(randomNumber());
 	pressSquare();
-	console.log('addOne', computerPattern);
-	// setTimeout(function () {
-	// 	pressSquare()
-	// }, 2000);
 }
 
 // That array then needs to be played by the computer on the buttons.
 
 function pressSquare() {
-	// let gameBoard = document.querySelector('.gameBoard');
-	// for (let i = 0; i < computerPattern.length; i++) {
-	// 	document
-	// 		.querySelector(`.box${computerPattern[i]}`)
-	// 		.classList.add(`layer${computerPattern[i]}`);
-	// 	setTimeout(function () {
-	// 		document
-	// 			.querySelector(`.box${computerPattern[i]}`)
-	// 			.classList.remove(`layer${computerPattern[i]}`);
-	// 	}, (i + 1)*1500);
-	// }
 	let j = 0;
 	const timer = setInterval(function () {
 		document
 			.querySelector(`.box${computerPattern[j]}`)
 			.classList.add(`layer${computerPattern[j]}`);
-		//
-		// startOsc(`buttonMethods.${computerPattern[j]}`);
 		setTimeout(function () {
 			document
 				.querySelector(`.box${computerPattern[j - 1]}`)
 				.classList.remove(`layer${computerPattern[j - 1]}`);
-			// stopOsc();
 		}, 1000);
 		j++;
 		if (j >= computerPattern.length) {
@@ -140,59 +83,44 @@ function pressSquare() {
 	}, 1500);
 }
 
-// Frequencies for Oscillator to be pulled from above function
-const buttonMethods = [
-	{ green: 164.8 },
-	{ red: 220.0 },
-	{ yellow: 277.2 },
-	{ blue: 329.6 },
-];
-
-// This builds the playerPattern array
-let gameBoard = document.querySelector('.gameBoard');
+// This part handles the effect of the squares lighting up as the player presses them and also builds the playerPattern array.
 
 gameBoard.addEventListener('click', handleClick);
 
 function handleClick(event) {
-	// console.log(event.target);
-	// playerPattern = [];
 	if (event.target.classList.contains('box1')) {
-		// document.querySelector('.box1').classList.add('layer1');
-		// setTimeout(function () {
-		// 	document.querySelector('.box1').classList.remove('layer1');
-		// }, 500);
+		document.querySelector('.box1').classList.add('layer1');
+		setTimeout(function () {
+			document.querySelector('.box1').classList.remove('layer1');
+		}, 500);
 		playerPattern.push(1);
-		// startOsc(164.8);
 	} else if (event.target.classList.contains('box2')) {
-		// document.querySelector('.box2').classList.add('layer2');
-		// setTimeout(function () {
-		// 	document.querySelector('.box2').classList.remove('layer2');
-		// }, 500);
+		document.querySelector('.box2').classList.add('layer2');
+		setTimeout(function () {
+			document.querySelector('.box2').classList.remove('layer2');
+		}, 500);
 		playerPattern.push(2);
-		// startOsc(220.0);
 	} else if (event.target.classList.contains('box3')) {
-		// document.querySelector('.box3').classList.add('layer3');
-		// setTimeout(function () {
-		// 	document.querySelector('.box3').classList.remove('layer3');
-		// }, 500);
+		document.querySelector('.box3').classList.add('layer3');
+		setTimeout(function () {
+			document.querySelector('.box3').classList.remove('layer3');
+		}, 500);
 		playerPattern.push(3);
-		// startOsc(277.2);
 	} else if (event.target.classList.contains('box4')) {
-		// document.querySelector('.box4').classList.add('layer4');
-		// setTimeout(function () {
-		// 	document.querySelector('.box4').classList.remove('layer4');
-		// }, 500);
+		document.querySelector('.box4').classList.add('layer4');
+		setTimeout(function () {
+			document.querySelector('.box4').classList.remove('layer4');
+		}, 500);
 		playerPattern.push(4);
-		// startOsc(329.6);
 	}
 	userClick++;
 	if (userClick === turnCounter + 1) {
-		console.log('check');
 		check();
 	}
 }
 
-// This part is where the buttons pressed are checked against the computer array
+// This part is where the playerPattern array is checked against the computerPattern array.
+
 function check() {
 	if (playerPattern.toString() !== computerPattern.toString()) {
 		loseGame();
@@ -207,6 +135,8 @@ function check() {
 	}
 }
 
+// This part is where the game ends in either defeat or victory.
+
 function loseGame() {
 	alert('That was pitiful!');
 }
@@ -216,48 +146,34 @@ function winGame() {
 	return;
 }
 
-// Audio Stuff
+// This part is where the oscillator is set up to play the sounds on the squares.
 
-// Green
-// E3
-// 164.8
+function startOsc(frequency) {
+	oscillator = context.createOscillator();
+	oscillator.type = 'square';
+	oscillator.frequency.value = frequency;
+	oscillator.start(0);
 
-// Red
-// A3
-// 220.0
+	oscillator.connect(context.destination);
+}
 
-// Yellow
-// C#4
-// 277.2
+function stopOsc() {
+	oscillator.stop(0);
+	oscillator.disconnect();
+}
 
-// Blue
-// E4
-// 329.6
-
-// Game end tone
-// A0
-// 27.5
-
-// D0
-//
-
-// B0
-//
+// This part lets each square have it's own unique tone when the player presses them.
 
 gameBoard.addEventListener('mousedown', handleMouseDown);
 
 function handleMouseDown(event) {
 	if (event.target.classList.contains('box1')) {
-		document.querySelector('.box1').classList.add('layer1');
 		startOsc(164.8);
 	} else if (event.target.classList.contains('box2')) {
-		document.querySelector('.box2').classList.add('layer2');
 		startOsc(220.0);
 	} else if (event.target.classList.contains('box3')) {
-		document.querySelector('.box3').classList.add('layer3');
 		startOsc(277.2);
 	} else if (event.target.classList.contains('box4')) {
-		document.querySelector('.box4').classList.add('layer4');
 		startOsc(329.6);
 	}
 }
@@ -269,45 +185,3 @@ function handleMouseUp(event) {
 		stopOsc();
 	}
 }
-
-const context = new AudioContext();
-let oscillator;
-let gain;
-
-function startOsc(frequency) {
-	oscillator = context.createOscillator();
-	oscillator.type = 'square';
-	oscillator.frequency.value = frequency;
-	oscillator.start(0);
-
-	oscillator.connect(context.destination);
-
-	gain = context.createGain();
-	gain.gain.value = 1;
-
-	// oscillator.connect(gain);
-	// gain.connect(context);
-}
-
-// function changeVolume(volume) {
-// 	let volume = parseFloat(params.volume);
-
-// 	if (isFinite(volume)) {
-// 		gainNode.gain.value = volume;
-// 	}
-// }
-
-function stopOsc() {
-	oscillator.stop(0);
-	oscillator.disconnect();
-}
-
-// -----------------------------------------------------------------
-// References
-// -----------------------------------------------------------------
-//
-// Awesome audio stuff learned in part through:
-// https://marcgg.com/blog/2016/11/01/javascript-audio/
-// https://middleearmedia.com/web-audio-api-basics/
-// https://middleearmedia.com/web-audio-api-oscillators/
-// https://middleearmedia.com/controlling-web-audio-api-oscillators/
